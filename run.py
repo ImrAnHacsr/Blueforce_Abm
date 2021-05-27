3c68a17bc535306fb57b41dd1eeba172ba1b7d23
Abdul Basit Minhas <52023076+Tech-abm@users.noreply.github.com>
2021-05-27 14:16:14 +0000

    Create run.py

---
+++ run.py
@@ -0,0 +1,22 @@
+import os, sys, ctypes
+
+if sys.version[0:3] != "3.9":
+  sys.exit("[+] anda harus menggunakan versi python 3.9, versi python anda sekarang : "+sys.version[0:3])
+
+brute = None
+
+try:
+  brute = __import__("brute")
+except Exception as E:
+  if ctypes.sizeof(ctypes.c_voidp) != 8:
+    [os.system(x) for x in ["pip install cython", "rm -rf *.so", "cythonize -i brute.c"]]
+    sys.exit("[*] silahkan jalankan ulang script ini.")
+
+if str(brute).startswith("<module ") == False:
+  sys.exit("[*] kesalahan pastikan kamu menginstall versi lengkap, https://github.com/dz-id/dmbf")
+
+if __name__ == "__main__":
+  try:
+    brute.main()
+  except Exception as E:
+    exit(str(E))
